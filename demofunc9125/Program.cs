800621d70deec82f606aa21ff83db93e5d380794
kathripdm <kadirpdm@gmail.com>
2024-11-02 21:08:34 +0530

    updated program

--- demofunc9125/Program.cs
+++ demofunc9125/Program.cs
@@ -1,13 +1,33 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+public static class DemoFunction
+{
+    [FunctionName("GetIndexHtml")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("Processing request for index.html.");
 
-host.Run();
+        var filePath = Path.Combine("wwwroot", "index.html");
+        if (File.Exists(filePath))
+        {
+            var content = await File.ReadAllTextAsync(filePath);
+            return new ContentResult
+            {
+                Content = content,
+                ContentType = "text/html"
+            };
+        }
+        else
+        {
+            return new NotFoundResult();
+        }
+    }
+}
