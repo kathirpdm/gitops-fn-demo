a12101dd806d5a3c7bb842c400e204067c4da6ac
kathripdm <kadirpdm@gmail.com>
2024-11-02 21:25:49 +0530

    update wflow

--- demofunc9125/Program.cs
+++ demofunc9125/Program.cs
@@ -1,33 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-public static class DemoFunction
-{
-    [FunctionName("GetIndexHtml")]
-    public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-        ILogger log)
-    {
-        log.LogInformation("Processing request for index.html.");
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .Build();
 
-        var filePath = Path.Combine("wwwroot", "index.html");
-        if (File.Exists(filePath))
-        {
-            var content = await File.ReadAllTextAsync(filePath);
-            return new ContentResult
-            {
-                Content = content,
-                ContentType = "text/html"
-            };
-        }
-        else
-        {
-            return new NotFoundResult();
-        }
-    }
-}
+host.Run();
